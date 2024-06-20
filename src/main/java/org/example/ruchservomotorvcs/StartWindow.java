3788e1292670d8c4f5e970e371d0257ef63905e5
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2024-06-20 15:54:38 +0300

    Login window added

--- src/main/java/org/example/ruchservomotorvcs/StartWindow.java
+++ src/main/java/org/example/ruchservomotorvcs/StartWindow.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -19,7 +21,7 @@ public class StartWindow extends Application {
         BorderPane root = createRootPane();
 
         // Создание вертикального контейнера для центрирования логотипа и кнопки
-        VBox centerBox = createCenterBox();
+        VBox centerBox = createCenterBox(root);
         root.setCenter(centerBox);
 
         // Создание контейнера для копирайта и центрирование его внизу
@@ -36,25 +38,25 @@ public class StartWindow extends Application {
 
     private BorderPane createRootPane() {
         BorderPane root = new BorderPane();
-        root.setStyle("-fx-background-color: #04060a;");
-
+        root.setStyle(
+                "-fx-background-color: #04060a;" +
+                        "-fx-border-color: #df6a1b; " +
+                        "-fx-border-width: 2px; " +
+                        "-fx-border-radius: 10px; " +
+                        "-fx-padding: 10px;" // Внутренний отступ
+        );
         return root;
     }
 
-    private VBox createCenterBox() {
+    private VBox createCenterBox(BorderPane root) {
         VBox centerBox = new VBox(20);
         centerBox.setAlignment(Pos.CENTER);
-        centerBox.setStyle(
-                "-fx-border-color: #df6a1b; " +
-                        "-fx-border-width: 2px; " +
-                        "-fx-border-radius: 10px; "
-        );
 
         ImageView logo = new ImageView(new Image("file:src/main/resources/org/example/ruchservomotorvcs/logo.png"));
         logo.setFitWidth(200);
         logo.setPreserveRatio(true);
 
-        Button loginButton = createLoginButton();
+        Button loginButton = createLoginButton(root);
 
         centerBox.getChildren().addAll(logo, loginButton);
 
@@ -73,7 +75,7 @@ public class StartWindow extends Application {
         return creditsText;
     }
 
-    private Button createLoginButton() {
+    private Button createLoginButton(BorderPane root) {
         Button loginButton = new Button("Войти");
         loginButton.setMinWidth(200);
         loginButton.setStyle(
@@ -82,10 +84,78 @@ public class StartWindow extends Application {
                         "-fx-text-fill: #df6a1b; " +
                         "-fx-border-color: #df6a1b; " +
                         "-fx-border-width: 2px; " +
-                        "-fx-border-radius: 10px; "
+                        "-fx-border-radius: 10px;" +
+                        "-fx-cursor: hand;"
         );
 
+        loginButton.setOnAction(event -> showLoginFields(root));
+
         return loginButton;
+    }
+
+    private void showLoginFields(BorderPane root) {
+        VBox loginBox = new VBox(10);
+        loginBox.setAlignment(Pos.CENTER);
+
+        TextField usernameField = new TextField();
+        usernameField.setMaxWidth(200);
+        usernameField.setPromptText("Логин");
+        usernameField.setStyle(
+                "-fx-font-size: 18px; " +
+                        "-fx-background-color: #04060a; " +
+                        "-fx-text-fill: #ffffff; " +
+                        "-fx-border-color: #df6a1b; " +
+                        "-fx-border-width: 2px; " +
+                        "-fx-border-radius: 10px;"
+        );
+
+        PasswordField passwordField = new PasswordField();
+        passwordField.setMaxWidth(200);
+        passwordField.setPromptText("Пароль");
+        passwordField.setStyle(
+                "-fx-font-size: 18px; " +
+                        "-fx-background-color: #04060a; " +
+                        "-fx-text-fill: #ffffff; " +
+                        "-fx-border-color: #df6a1b; " +
+                        "-fx-border-width: 2px; " +
+                        "-fx-border-radius: 10px;"
+        );
+
+        Button submitButton = new Button("Войти");
+        submitButton.setMinWidth(200);
+        submitButton.setStyle(
+                "-fx-font-size: 18px; " +
+                        "-fx-background-color: #df6a1b; " +
+                        "-fx-text-fill: #04060a; " +
+                        "-fx-background-radius: 10px;" +
+                        "-fx-cursor: hand;"
+        );
+
+        Button backButton = new Button("<");
+        backButton.setMinWidth(40);
+        backButton.setStyle(
+                "-fx-font-size: 18px; " +
+                        "-fx-background-color: #04060a; " +
+                        "-fx-text-fill: #df6a1b; " +
+                        "-fx-border-color: #df6a1b; " +
+                        "-fx-border-width: 2px; " +
+                        "-fx-border-radius: 10px;" +
+                        "-fx-cursor: hand;"
+        );
+
+        backButton.setOnAction(event -> {
+            VBox centerBox = createCenterBox(root);
+            root.setCenter(centerBox);
+            root.setTop(null); // Удалить кнопку Назад
+        });
+
+        usernameField.setFocusTraversable(false);
+        passwordField.setFocusTraversable(false);
+
+        loginBox.getChildren().addAll(usernameField, passwordField, submitButton);
+
+        root.setCenter(loginBox);
+        root.setTop(backButton);
     }
 
     public static void main(String[] args) {
